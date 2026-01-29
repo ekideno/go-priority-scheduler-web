41e8496a1003a17518a4e1d963b5269411bf6304
Denchik <denchik.git.hub@proton.me>
2026-01-29 19:26:15 +0300

    feat(web): simple visualization

--- internal/web/scheduler/types.go
+++ internal/web/scheduler/types.go
@@ -6,9 +6,9 @@ import (
 )
 
 type Job struct {
-	ID       int64
-	Name     string
-	Priority int
-	Duration time.Duration
+	ID       int64                                      `json:"id"`
+	Name     string                                     `json:"name"`
+	Priority int                                        `json:"priority"`
+	Duration time.Duration                              `json:"duration"`
 	Execute  func(ctx context.Context, d time.Duration) `json:"-"`
 }
